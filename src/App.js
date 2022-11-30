329ce23eb60bde95575485de36f7e66098977dc9
Akshaya218 <72906066+Akshaya218@users.noreply.github.com>
2022-11-30 11:08:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import "./index.css";
+import React from "react";
+import SortingVisualizer from "./components/SortingVisualizer";
+
+//calling the sortingvisualizer component
+function App() {
+  return (
+    <div>
+      <SortingVisualizer />
+    </div>
+  );
+}
+
+export default App;
